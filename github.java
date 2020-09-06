65c4fd5bae80b61b8a08920cb7b01d75d26aa9bc
Vignesh K <30373348+villuvicky@users.noreply.github.com>
2020-09-06 09:37:33 +0530

    Update to check jenkins run the code

--- github.java
+++ github.java
@@ -3,6 +3,7 @@ public class github {
  public static void main(String[] args) {
   // TODO Auto-generated method stub
     System.out.println("Hello Vicky");
+  System.out.println("Hi Vicky");
 
  }
 
